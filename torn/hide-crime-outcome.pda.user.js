f2cb80d825bfa0c2a54f20e68c55c0ca84f8244b
Kwack <kwack@kwack.dev>
2024-03-12 23:45:23 +0800

    Fix version number

--- torn/hide-crime-outcome.pda.user.js
+++ torn/hide-crime-outcome.pda.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Crime Outcome
 // @namespace    dev.kwack.torn.hide-crime-results
-// @version      0.0.3
+// @version      1.0.0
 // @description  Hides the crime outcome panel for quick clicking. Quick and dirty script
 // @author       Kwack [2190604]
 // @match        https://www.torn.com/loader.php?sid=crimes*
