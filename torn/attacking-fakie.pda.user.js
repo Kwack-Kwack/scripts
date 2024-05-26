3d72de2d0afecaa15772f40e9519d7d4b2658f09
Kwack <kwack@kwack.dev>
2024-05-26 21:19:35 +0800

    attacking-fakie initial upload

---
+++ torn/attacking-fakie.pda.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Attacking Fakie
+// @namespace    dev.kwack.torn.attacking-fakie
+// @version      0.0.1
+// @description  Inverts the attacking layout horizontally, placing buttons on the right side of the screen
+// @author       Kwack [2190604]
+// @match        https://www.torn.com/loader.php
+// @run-at 		 document-end
+// ==/UserScript==
+if (new URL(document.location).searchParams.get("sid") !== "attack") return; // Only trigger on the attack page
+(() => {
+	"use strict";
+	waitForElement("#attacker > div[class*=playerArea_]").then((element) => {
+		element.css("flex-direction", "row-reverse");
+	});
+
+	function waitForElement(selector) {
+		return new Promise((res) => {
+			const getElement = () => {
+				const element = $(selector);
+				if (element.length) {
+					res(element);
+				} else {
+					setTimeout(getElement, 100);
+				}
+			}
+			getElement();
+		})
+	}
+})();
